057c7af38733ebd600834036a2e9bbdea4d82307
HromadaStefan <if21b184@technikum-wien.at>
2023-11-05 18:55:21 +0100

    Unit Tests funktionieren. Validation Unit-Test fehlt

--- apps/paperless-rest/src/test/java/at/fhtw/swkom/paperless/services/validation/DocumentValidationTest.java
+++ apps/paperless-rest/src/test/java/at/fhtw/swkom/paperless/services/validation/DocumentValidationTest.java
@@ -4,6 +4,7 @@ import at.fhtw.swkom.paperless.domain.DocumentsDocument;
 import at.fhtw.swkom.paperless.domain.DocumentsDocumentTags;
 import at.fhtw.swkom.paperless.repos.DocumentsDocumentRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
