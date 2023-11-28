5d5ba2abd6349640f62136fcdbde9c96b90de0a3
Patrick Weiss <p4trick.weiss@gmail.com>
2023-11-28 10:05:49 +0100

    automatic message queue creation rabbitmq

--- apps/paperless-rest/src/main/java/at/technikumwien/swkom/paperlessrest/config/RabbitMQConfig.java
+++ apps/paperless-rest/src/main/java/at/technikumwien/swkom/paperlessrest/config/RabbitMQConfig.java
@@ -4,24 +4,27 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
     public static final String DOCUMENT_QUEUE = "documents";
 
+    @Bean
     public Queue documentQueue() {
         return new Queue(DOCUMENT_QUEUE, false);
     }
 
+    @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory("rabbitmq");
-        connectionFactory.setPort(5672);
         connectionFactory.setUsername("admin");
         connectionFactory.setPassword("admin-password");
         return connectionFactory;
     }
 
+    @Bean
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
         rabbitTemplate.setDefaultReceiveQueue(DOCUMENT_QUEUE);
